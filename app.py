3e8028fc2b746fa045a4c1c4479ad9e689a5c095
jamesw8 <jameswong8@gmail.com>
2017-11-17 18:49:16 -0500

    Added flask

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+	return render_template('index.html')
+
+
+
+# Run server
+if __name__ == '__main__':
+	port = int(os.environ.get('PORT', 5000))
+	app.run(debug=True, host='0.0.0.0', port=port)
